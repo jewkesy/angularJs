488c62016f9787c286db8e1faddc48e84be6a4b2
Daryl Jewkes <github@daryljewkes.com>
2014-10-17 12:20:11 +0100

    corrected Add Some Control

--- basics/js/todo.js
+++ basics/js/todo.js
@@ -3,11 +3,12 @@ angular.module('todoApp', [])
     $scope.todos = [
       {text:'learn angular', done:true},
       {text:'build an angular app', done:false}];
-    ];
+
     $scope.addTodo = function() {
       $scope.todos.push({text:$scope.todoText, done:false});
       $scope.todoText = '';
     };
+
     $scope.remaining = function() {
       var count = 0;
       angular.forEach($scope.todos, function(todo) {
@@ -15,10 +16,11 @@ angular.module('todoApp', [])
       });
       return count;
     };
-    $scope.archive = function(){
+
+    $scope.archive = function() {
       var oldTodos = $scope.todos;
       $scope.todos = [];
-      angular.forEach(oldTodos, function(todo){
+      angular.forEach(oldTodos, function(todo) {
         if (!todo.done) $scope.todos.push(todo);
       });
     };
